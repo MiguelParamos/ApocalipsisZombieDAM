f6e982c79fdf6bf7b4862dc0237f42058391e14d
adri323 <adrianmat775@gmail.com>
2019-11-18 09:49:54 +0100

    Create FuncionesAJEA.java
    nombre personaje y puntos

---
+++ Apocalipsis Zombie DAM/src/adrian/javier/eduardo/alvaro/FuncionesAJEA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adrian.javier.eduardo.alvaro;
+
+/**
+ *
+ * @author cenec
+ */
+public class FuncionesAJEA {
+    
+    public static String nombre="AJEA";
+    public static byte velocidad=60;
+    public static byte fuerza=80;
+    public static byte intuicion =40;
+    public static byte percepcion=20;
+}
