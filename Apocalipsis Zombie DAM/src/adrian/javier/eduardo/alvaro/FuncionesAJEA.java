58025a8dc83fbf3c6e7a8acf160a72240ffc8068
adri323 <adrianmat775@gmail.com>
2019-11-18 11:09:58 +0100

    funcion coherencia
    deccripcion

--- Apocalipsis Zombie DAM/src/adrian/javier/eduardo/alvaro/FuncionesAJEA.java
+++ Apocalipsis Zombie DAM/src/adrian/javier/eduardo/alvaro/FuncionesAJEA.java
@@ -6,17 +6,30 @@
 package adrian.javier.eduardo.alvaro;
 
 /**
- *Aqui se definen los puntos repartidos al personaje.
+ * Aqui se definen los puntos repartidos al personaje.
+ *
  * @author adrian
  * @author javier
  * @author eduardo
  * @author alvaro
  */
 public class FuncionesAJEA {
-    
-    public static String nombre="AJEA";//Nombre del personaje
-    public static byte velocidad=60;//velocidad 0-100 jugador
-    public static byte fuerza=80;//fuerza 0-100 jugador
-    public static byte intuicion =40;//intuicion 0-100 jugador
-    public static byte percepcion=20;//percepcion 0-100 jugador
+
+    public static String nombre = "AJEA";//Nombre del personaje
+    public static byte velocidad = 60;//velocidad 0-100 jugador
+    public static byte fuerza = 80;//fuerza 0-100 jugador
+    public static byte intuicion = 40;//intuicion 0-100 jugador
+    public static byte percepcion = 20;//percepcion 0-100 jugador
+
+    public static boolean comprobarCoherencia(byte velocidad, byte fuerza, byte intuicion, byte percepcion) {
+
+        int total = velocidad + fuerza + intuicion + percepcion;
+        if (total == 200) {
+
+            return true;
+        } else {
+           return false;
+        }
+
+    }
 }
