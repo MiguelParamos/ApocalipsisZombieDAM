864d3818f3bd479d813dc9f136b399467f0beffc
fparra00 <fparraoboe@gmail.com>
2019-11-18 11:19:26 +0100

    Update FuncionesFIJA.java

--- Apocalipsis Zombie DAM/src/juan/fernando/alberto/ivan/FuncionesFIJA.java
+++ Apocalipsis Zombie DAM/src/juan/fernando/alberto/ivan/FuncionesFIJA.java
@@ -5,17 +5,38 @@
  */
 package juan.fernando.alberto.ivan;
 
+import java.util.Scanner;
+
 /**
  * Esta clase describirá los atributos y el nombre de nuestro personaje
+ *
  * @author ivang
  * @author fparra
  * @author juan
  * @author alberto
  */
 public class FuncionesFIJA {
-    public static String nombre="Luis Alberto"; //Nombre del Jugador
-    public static byte velocidad=50; //Velocidad 0-100 del jugador
-    public static byte fuerza=90;  //Fuerza 0-100 del jugador
-    public static byte intuicion=30; //Intuición 0-100 del jugador
-    public static byte percepcion=30; //Percepción 0-100 del jugador
+
+    public static String nombre = "Luis Alberto"; //Nombre del Jugador
+    public static byte velocidad = 50; //Velocidad 0-100 del jugador
+    public static byte fuerza = 90;  //Fuerza 0-100 del jugador
+    public static byte intuicion = 30; //Intuición 0-100 del jugador
+    public static byte percepcion = 30; //Percepción 0-100 del jugador
+    
+    /**
+     * A FUNCIÓN RECOGE LAS VARIABLES VELOCIDAD, FUERZA, INTUICION Y PERCEPCION, Y DEVUELVE VERDADERO EN CASO DE QUE 
+       SEA EQUIVALENTE A 200, EN CASO CONTRARIO, DEVUELVE FALSO 
+     * @param velocidad
+     * @param fuerza
+     * @param intuicion
+     * @param percepcion
+     * @return 
+     */
+    public static boolean sumar200(Byte velocidad, Byte fuerza, Byte intuicion, Byte percepcion) { 
+        boolean ej = true;
+        if (velocidad + fuerza + intuicion + percepcion == 200) { //SI TODOS LOS PARAMETROS SUMAN 200, CUMPLE LAS CONDICIONES   
+        }
+        return ej;
+
+    }
 }
