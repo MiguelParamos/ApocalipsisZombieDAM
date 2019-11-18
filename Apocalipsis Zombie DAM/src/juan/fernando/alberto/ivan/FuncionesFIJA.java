acd9a2bfd4e2cd172966441eba06a1c67a371368
fparra00 <fparraoboe@gmail.com>
2019-11-18 10:54:02 +0100

    Update FuncionesFIJA.java

--- Apocalipsis Zombie DAM/src/juan/fernando/alberto/ivan/FuncionesFIJA.java
+++ Apocalipsis Zombie DAM/src/juan/fernando/alberto/ivan/FuncionesFIJA.java
@@ -6,13 +6,16 @@
 package juan.fernando.alberto.ivan;
 
 /**
- *
+ *  Esta función describirá los atributos y el nombre de nuestro personaje
  * @author ivang
+ * @author fparra
+ * @author juan
+ * @author alberto
  */
 public class FuncionesFIJA {
-    public static String nombre="Luis Alberto";
-    public static byte velocidad=50;
-    public static byte fuerza=90;
-    public static byte intuicion=30;
-    public static byte percepcion=30;
+    public static String nombre="Luis Alberto"; //Nombre del Jugador
+    public static byte velocidad=50; //Velocidad 0-100 del jugador
+    public static byte fuerza=90;  //Idem Fuerza
+    public static byte intuicion=30; //Idem Intuicion
+    public static byte percepcion=30; //Idem Percepcion
 }
