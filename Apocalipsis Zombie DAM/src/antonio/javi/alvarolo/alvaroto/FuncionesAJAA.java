aab9b44f68513d14a968ae511af4a698c12271a2
alvaro-to <alvaro-to@users.noreply.github.com>
2019-11-18 09:55:25 +0100

    Funcion
    Grupo Antonio,Javi,AlvaroLo,AlvaroTo

---
+++ Apocalipsis Zombie DAM/src/antonio/javi/alvarolo/alvaroto/FuncionesAJAA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antonio.javi.alvarolo.alvaroto;
+
+/**
+ *
+ * @author mikim
+ */
+public class FuncionesAJAA {
+
+    public static String nombre = "Aitor Tilla";
+    public static byte velocidad =75 ;
+    public static byte fuerza =25 ;
+    public static byte intuición =25 ;
+    public static byte percepción =75 ;
+
+    
+}
