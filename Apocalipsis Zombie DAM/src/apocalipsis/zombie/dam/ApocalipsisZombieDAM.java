0f2637eb077b9b47e361b8b0ae1e7f7078360ff7
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2019-11-18 10:26:06 +0100

    versión para generar conflictos
    versión para generar conflictos

--- Apocalipsis Zombie DAM/src/apocalipsis/zombie/dam/ApocalipsisZombieDAM.java
+++ Apocalipsis Zombie DAM/src/apocalipsis/zombie/dam/ApocalipsisZombieDAM.java
@@ -5,6 +5,8 @@
  */
 package apocalipsis.zombie.dam;
 
+import adrian.javier.eduardo.alvaro.FuncionesAJEA;
+
 /**
  *
  * @author Ccenec
@@ -16,6 +18,7 @@ public class ApocalipsisZombieDAM {
      */
     public static void main(String[] args) {
         System.out.println("Cereeeebrooos!");
+        System.out.println("Más Cerebros");
     }
     
 }
