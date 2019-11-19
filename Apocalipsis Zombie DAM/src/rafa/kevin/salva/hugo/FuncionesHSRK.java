1a45992df3faecc11d65f112dd0af6aa7ca6f4af
rafacarrion7 <rcponce2000@gmail.com>
2019-11-19 09:44:29 +0100

    Update FuncionesHSRK.java

--- Apocalipsis Zombie DAM/src/rafa/kevin/salva/hugo/FuncionesHSRK.java
+++ Apocalipsis Zombie DAM/src/rafa/kevin/salva/hugo/FuncionesHSRK.java
@@ -6,30 +6,58 @@
 package rafa.kevin.salva.hugo;
 
 /**
- *  En esta clase se define los puntos repartidos de nuestro personaje que se llama Abascal
+ * En esta clase se define los puntos repartidos de nuestro personaje que se
+ * llama Abascal
+ *
  * @author Salva
  * @author Rafa
  * @author Hugo
  * @author Kevin
  */
 public class FuncionesHSRK {
+
     public static String nombre = "Abascal";//Nombre del jugador 0-100
-    public static byte velocidad=70;//Velocidad del jugador 0-100
-    public static byte fuerza=70;//Fuerza del jugador 0-100
-    public static byte intuicion=30;//Intuicion del jugador 0-100
-    public static byte percepcion=30;//Percepcion del jugador 0-100
-    
+    public static byte velocidad = 70;//Velocidad del jugador 0-100
+    public static byte fuerza = 70;//Fuerza del jugador 0-100
+    public static byte intuicion = 30;//Intuicion del jugador 0-100
+    public static byte percepcion = 30;//Percepcion del jugador 0-100
+
     /**
-     * Esta función comprueba si la suma de los cuatro parámetros es
-     * igual a 200 y devuelve un boolean al respecto.
-     * 
+     * Esta función comprueba si la suma de los cuatro parámetros es igual a 200
+     * y devuelve un boolean al respecto.
+     *
      * @param velocidad es la velocidad que tiene el personaje de 0-100
      * @param fuerza es la fuerza que tiene el personaje de 0-100
      * @param intuicion es la intuicion que tiene el personaje de 0-100
      * @param percepcion es la percepcion que tiene el personaje de 0-100
      * @return Devuelve un boolean verdadero si suman 200, falso si no.
      */
-    public static boolean comprobarCoherencia(byte velocidad,byte fuerza,byte intuicion,byte percepcion){
-        return velocidad+fuerza+intuicion+percepcion == 200;    
+    public static boolean comprobarCoherencia(byte velocidad, byte fuerza, byte intuicion, byte percepcion) {
+        return velocidad + fuerza + intuicion + percepcion == 200;
+    }
+
+   /** En esta funcion vemos la acción que se va a realizar. Dependiendo de la percepcion del personaje veremos una 
+    * cosa u otra.
+    * 
+    * @param turno El numero de turno.
+    * @param accion Lo que el personaje va a hacer. En este caso mirar por la ventana.
+    * @return Devuelve lo que sucede dependiendo de la percepción.
+    */
+    public static String accionReaccion(byte turno, String accion) {
+        if (turno == 1) {
+            if (accion.equals("mirar por la ventana")) {
+                if (percepcion < 50) {
+                    return ("No veo nada");
+
+                } else if (percepcion >= 50 && percepcion < 80) {
+                    return ("Veo alboroto en general");
+
+                } else{
+                    return ("Veo gente corriendo en la plaza de la derecha");
+                }
+            }
+        }
+        return null; 
+
     }
 }
