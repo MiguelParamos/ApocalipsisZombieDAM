f00b001f63ea25dc4a0a6d8b5fd7b39b8ad82a6d
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2019-11-21 14:13:10 +0100

    Planteado turno 5
    Planteado turno 5 en el main

--- Apocalipsis Zombie DAM/src/adrian/javier/eduardo/alvaro/FuncionesAJEA.java
+++ Apocalipsis Zombie DAM/src/adrian/javier/eduardo/alvaro/FuncionesAJEA.java
@@ -115,8 +115,8 @@ public static String accionReaccion(byte turno, String accion) {
             }
         }
         else if (turno == 4) {
-            if (accion.equals("Piden a luis alberto trabajar juntos y cogen extintores")) {
-                return "alianza y cogen extintores";
+            if (accion.equals("alianza y cogen extintores")) {
+                return "Piden a luis alberto trabajar juntos y cogen extintores";
 
             }
         }
