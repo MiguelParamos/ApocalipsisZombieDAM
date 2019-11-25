9614d699f1696e072d0d99a091e1216ac1272c35
hugonzalezb <57891133+hugonzalezb@users.noreply.github.com>
2019-11-25 09:06:57 +0100

    Update FuncionesHSRK.java

--- Apocalipsis Zombie DAM/src/rafa/kevin/salva/hugo/FuncionesHSRK.java
+++ Apocalipsis Zombie DAM/src/rafa/kevin/salva/hugo/FuncionesHSRK.java
@@ -92,7 +92,7 @@ public class FuncionesHSRK {
      * @return Devuelve si finalmente logramos quitarle el objeto
      */
     public static String robarObjeto(String nombre_Atracar, byte mi_vel, byte su_vel, byte mi_fuerza, byte su_fuerza){
-        if(mi_vel >= su_vel || mi_fuerza >= su_fuerza){
+        if(mi_vel >= su_vel && mi_fuerza >= su_fuerza){
             return "Le quito el objeto a Aitor Torilla";
         } else {
             return "No logro quitárselo. Me pregunta ¿Qué estás haciendo?";
