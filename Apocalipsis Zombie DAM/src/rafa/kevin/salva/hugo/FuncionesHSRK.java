e47f64cefa3791403c8ec9b57ba6a81b3e985e82
hugonzalezb <57891133+hugonzalezb@users.noreply.github.com>
2019-11-25 08:51:54 +0100

    Update FuncionesHSRK.java

--- Apocalipsis Zombie DAM/src/rafa/kevin/salva/hugo/FuncionesHSRK.java
+++ Apocalipsis Zombie DAM/src/rafa/kevin/salva/hugo/FuncionesHSRK.java
@@ -81,7 +81,17 @@ public class FuncionesHSRK {
         return null;
            
     }
-    
+    /**
+     * Esta función valora la posibilidad de robarle el teléfono a uno de los personajes, comparando con la velocidad y la fuerza de ambos.
+     * 
+     * @param turno turno en el que se realiza la accion
+     * @param nombre_Atracar personaje al que atacamos
+     * @param mi_vel mi velocidad
+     * @param su_vel su velocidad
+     * @param mi_fuerza mi fuerza
+     * @param su_fuerza su fuerza
+     * @return 
+     */
     public static String robarObjeto(String nombre_Atracar, byte mi_vel, byte su_vel, byte mi_fuerza, byte su_fuerza){
         if(mi_vel >= su_vel && mi_fuerza >= su_fuerza){
             return "Le quito el objeto a Aitor Torilla";
